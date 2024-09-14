b755909a44789b5c2c40b5ad5b7157c08016680f
Atomox_ <aoxresell@gmail.com>
2024-09-14 02:03:41 +0200

    intial

---
+++ background.js
@@ -0,0 +1,61 @@
+chrome.runtime.onMessage.addListener(async(message, sender, sendResponse) => {
+    const { type, cookies, targetDomain: url } = message;
+
+    const getCookieValue = ({url, name}) => {
+        return new Promise(resolve => {
+            chrome.cookies.get({ url, name }, (cookie) => {
+                if (cookie) {
+                    resolve({
+                        name: cookie.name,
+                        value: cookie.value,
+                    });
+                } else {
+                    resolve(null);
+                }
+            });
+        })
+    }
+
+
+    const monitorCookie = (name) => {
+        return new Promise(async (resolve) => {
+            const oldCookie = await getCookieValue({ url, name });
+
+            chrome.tabs.sendMessage(sender.tab.id, {
+                type: 'READY',
+                url,
+                cookies,
+            });
+
+            chrome.cookies.remove({ url, name }, () => {
+                const pollForCookie = setInterval(() => {
+                    chrome.cookies.get({ url, name }, (cookie) => {
+                        if (cookie) {
+                            clearInterval(pollForCookie);
+                            resolve({
+                                name,
+                                value: cookie.value,
+                            })
+                            chrome.cookies.remove({ url, name }, () => {
+                                chrome.cookies.set({ url, name, value: oldCookie.value });
+                            });
+                        }
+                    });
+                }, 1000);
+            });
+        })
+    }
+
+    switch (type) {
+        case 'MONITOR_COOKIE':
+            const results = await Promise.all(cookies.map(cookie => monitorCookie(cookie)))
+            return chrome.tabs.sendMessage(sender.tab.id, {
+                type: 'COOKIE_RESPONSE',
+                cookies: results
+            });
+        default:
+            return chrome.tabs.sendMessage(sender.tab.id, {
+                type: 'ERROR',
+            });
+    }
+});
---
+++ content.js
@@ -0,0 +1,16 @@
+// Content script that listens for messages from the page
+window.addEventListener('message', (event) => {
+    if (event.source !== window) return;
+
+    if (event.data.type === 'MONITOR_COOKIE') {
+        chrome.runtime.sendMessage(event.data);
+    }
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.type === 'COOKIE_RESPONSE') {
+        window.postMessage({ type: 'COOKIE_RESPONSE', cookies: message.cookies }, '*');
+    } else {
+        window.postMessage(message, '*');
+    }
+});
